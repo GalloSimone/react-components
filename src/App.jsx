698c69279c4f129dd5744f85d6531a1751d43a7b
GalloSimone <gallosimonecz@gmail.com>
2024-11-28 17:56:51 +0100

    insert lower case import postCard

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "./components/Header.jsx";
-import PostCard from "./components/PostCard.jsx";
+import PostCard from "./components/postCard.jsx";
 import Footer from "./components/footer.jsx";
 import './App.css'
 
